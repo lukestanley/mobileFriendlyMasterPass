c7ae2f3c9153b3e04d9046494e17677f9a4a43d5
Luke Stanley <luke.stanley@gmail.com>
2015-11-08 03:39:49 +0000

    Tidied up syntax a bit

---
+++ test.py
@@ -0,0 +1,7 @@
+from mobileFriendlyMasterPass import make_password
+
+password = make_password('this-is-a-silly-pass','wordpress')
+
+print (password)
+
+assert (password == 'hirsute.regularity.confab.gazelle.gnome.6512')
